e85e8b199498b626b8d00b45254f7971506ccd7e
Robert Ogardo <rogardo53143@liceo.edu.ph>
2025-08-01 11:26:33 +0800

    lab-activity-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Main Branch!");
+    static int number = 0;
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("PLEASE INPUT 5 NUMBERS");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i+ ": " );
+            number = number+scanner.nextInt();
+
+        }
+        System.out.println("");
+        System.out.println("Total Number: "+number);
+
+        scanner.close();
     }
 }
