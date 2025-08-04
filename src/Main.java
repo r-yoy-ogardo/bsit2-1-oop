942ea45308938108b94ba2d864704facd5869004
Robert Ogardo <rogardo53143@liceo.edu.ph>
2025-08-04 22:35:58 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+
+        Book book1 = new Book("The Fellowship of the Ring", "John Ronald Reuel Tolkien", 479);
+        Book book2 = new Book("The Two Towers", "John Ronald Reuel Tolkien", 415);
+        Book book3 = new Book("The Return of the King", "John Ronald Reuel Tolkien", 347);
+
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book1.displayInfo();
+        book1.returnBook();
+        book1.displayInfo();
+
+        book2.borrowBook();
+        book2.displayInfo();
+        book2.returnBook();
+        book2.displayInfo();
+
+        book3.borrowBook();
+        book3.displayInfo();
+        book3.returnBook();
+        book3.displayInfo();
     }
 }
