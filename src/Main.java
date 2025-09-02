d5bafe3d4527171c9b9dc85e0d36aa65c620fdb0
Robert Ogardo <rogardo53143@liceo.edu.ph>
2025-09-03 00:11:26 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         LibraryManager library = new LibraryManager();
-        System.out.println("== Library Management System ==");
+        System.out.println("=== Library Management System ===");
+        System.out.println();
 
         library.showBooks();
         library.addBook();
         library.removeBook();
+
+        System.out.println("Program completed successfully.");
     }
 }
