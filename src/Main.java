b5a02a53d1925e57840a2f6d6b93d1d37a003712
Robert Ogardo <rogardo53143@liceo.edu.ph>
2025-09-15 10:17:04 +0800

    lab-activity-9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+        PetService service = new PetService();
+
+        System.out.println("Basic Checkup: $" + service.calculateFee());
+        System.out.println("Checkup with vaccination: $" + service.calculateFee(true));
+        System.out.println("Full Service: $" + service.calculateFee(true, true));
+        System.out.println("Emergency: $" + service.calculateFee("emergency"));
+        System.out.println("\n");
+
+        System.out.println("Welcome to the Pet Clinic!");
+        System.out.println("============================");
+        Pet dog = new Dog("Buddy", 3);
+        Pet cat = new Cat("Whiskers", 2);
+        Pet bird = new Bird("Tweety", 1);
+
+        dog.displayInfo();
+        cat.displayInfo();
+        bird.displayInfo();
+
+        System.out.println("Training Session Started!");
+        System.out.println("=========================");
+        trainPet((Trainable) dog);
+        trainPet((Trainable) bird);
+    }
+
+    public static void trainPet(Trainable pet) {
+        pet.performTrick();
     }
 }
